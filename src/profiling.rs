64b9c31c14918e5a6232127f86e1af5e6b021a3f
Tilman Dietzel <i@nmlt.de>
2022-11-22 18:47:32 +0100

    Add Checkbox and Radio

--- src/profiling.rs
+++ src/profiling.rs
@@ -11,57 +11,83 @@ struct Form {
     dataset: Option<String>,
 }
 
-struct CheckboxData {
-    label: String,
-    value: String,
-}
-
+#[derive(Clone, Properties, PartialEq)]
 struct InputCheckboxProps {
-    
-
+    label: String,
 }
 
 #[function_component]
-fn InputCheckbox() -> Html {
+fn InputCheckbox(InputCheckboxProps { label }: &InputCheckboxProps) -> Html {
     let (store, dispatch) = use_store::<Form>();
     let onchange = dispatch.input_mut(|s, value| {
         s.sort_data = value;
     });
 
     html! {
-        <>
-        <p>{store.sort_data.checked()}</p>
-        <input type="checkbox" {onchange} />
-        </>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" {onchange} />
+            <label class="form-check-label">{label.clone()}</label>
+        </div>
     }
 }
 
+#[derive(Clone, PartialEq)]
+struct RadioData {
+    label: String,
+    value: String,
+}
+impl RadioData {
+    fn new(label: &str, value: &str) -> Self {
+        RadioData {
+            label: String::from(label),
+            value: String::from(value),
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Properties)]
+struct InputRadioProps {
+    data: Vec<RadioData>,
+    title: String,
+}
+
 #[function_component]
-fn InputRadio() -> Html {
+fn InputRadio(InputRadioProps { data, title }: &InputRadioProps) -> Html {
     let (store, dispatch) = use_store::<Form>();
     let onchange = dispatch.input_mut(|s, value| {
         s.dataset = Some(value);
     });
-
+    let options = data.iter().map(|RadioData {label, value}| {
+        html!{
+            <div class="form-check">
+                <input class="form-check-input" onchange={onchange.clone()} type="radio" id={format!("radio-{}", value)} name="dataset" value={value.clone()} />
+                <label class="form-label" for={format!("radio-{}", value)}>{label.clone()}</label>
+            </div>
+        }
+    });
     html! {
-        <>
-        <p>{store.dataset.clone().unwrap_or_default()}</p>
-        <input onchange={onchange.clone()} type="radio" id="dog" name="animal" value="cat"/ >
-        <label for="cat">{ "cat" }</label><br />
-        <input onchange={onchange.clone()} type="radio" id="cat" name="animal" value="dog"/ >
-        <label for="dog">{"dog"}</label><br />
-        </>
+        <fieldset class="row mb-3 col-md">
+            <legend class="form-label">{title}</legend>
+            <div class="col-sm-10">
+                {for options}
+            </div>
+        </fieldset>
     }
 }
 
 #[function_component(Profiling)]
 pub fn profiling() -> Html {
-
+    let datasets = vec![RadioData::new("cache-exceed", "cache-exceed"), RadioData::new("cache-fit", "cache-fit")];
     html!{
         <div>
             <h2>{"Profiling"}</h2>
             <Navigation />
-            
+            <form class="row g-3">
+                <div class="col-md">
+                    <InputCheckbox label={"Sort data"} />
+                    <InputRadio data={datasets} title={"Dataset"}/>
+                </div>
+            </form>
         </div>
     }
 }
