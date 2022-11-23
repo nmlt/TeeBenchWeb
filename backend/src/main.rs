72b3fff5862181d0fb3b88f053d161c448def98a
Tilman Dietzel <i@nmlt.de>
2022-11-23 13:43:43 +0100

    Enable frontend and backend to work together

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,11 +2,14 @@ use axum::{
     routing::get,
     Router,
 };
+use axum_extra::routing::SpaRouter;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, Andor!" }));
+    let spa = SpaRouter::new("/assets", "../dist");
+    let app = Router::new()
+        .merge(spa)
+        .route("/api/test", get(|| async {"Test successful!"}));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
