decb59eeff7c6aae82bf2ff607d62be2603b62e7
Tilman Dietzel <i@nmlt.de>
2023-02-27 17:40:08 +0100

    Move removing from queue to after the job has finished
    Because otherwise the frontend will not know of the currently
    running job when it is reloaded, therefore becoming out of sync
    with the backend.
    
    Should close #21

--- backend/src/lib/lib.rs
+++ backend/src/lib/lib.rs
@@ -49,6 +49,60 @@ async fn compile_and_run(conf: ProfilingConfiguration) -> Report {
     Report::default()
 }
 
+#[instrument(skip(queue, oneshot_tx, queue_tx))]
+async fn work_on_queue(
+    queue: Arc<Mutex<VecDeque<ProfilingConfiguration>>>,
+    oneshot_tx: oneshot::Sender<()>,
+    queue_tx: mpsc::Sender<Job>,
+) {
+    fn peek_queue(
+        queue: Arc<Mutex<VecDeque<ProfilingConfiguration>>>,
+    ) -> Option<ProfilingConfiguration> {
+        let guard = queue.lock().unwrap();
+        guard.front().map(ProfilingConfiguration::clone)
+    }
+    // TODO Do not clone the queue. It should be always the same arc, but how to make it work with the borrow checker?
+    // Probably by making the above function a closure, which would also be more idiomatic.
+    while let Some(current_conf) = peek_queue(queue.clone()) {
+        info!("Working on {current_conf:#?}...");
+        let report = compile_and_run(current_conf.clone()).await;
+        info!("Process completed with {report:?}.");
+        let finished_job = Job::Finished {
+            config: current_conf,
+            submitted: OffsetDateTime::now_utc(), // TODO Fix this.
+            runtime: Duration::new(5, 0), // TODO Get actual runtime from teebench output.
+            result: Ok(ReportWithFindings {
+                report: report,
+                findings: Vec::new(),
+            }),
+        };
+        {
+            let mut guard = queue.lock().unwrap();
+            guard.pop_front();
+        }
+        queue_tx.send(finished_job).await.unwrap();
+    }
+    oneshot_tx.send(()).unwrap();
+}
+
+async fn receive_confs(
+    queue: Arc<Mutex<VecDeque<ProfilingConfiguration>>>,
+    rx: Arc<tokio::sync::Mutex<mpsc::Receiver<ProfilingConfiguration>>>,
+    queue_tx: mpsc::Sender<Job>,
+) {
+    let mut rx_guard = rx.lock().await;
+    match rx_guard.recv().await {
+        Some(conf) => {
+            info!("New task came in!");
+            let running_job = Job::Running(conf.clone());
+            queue_tx.send(running_job).await.unwrap();
+            let mut guard = queue.lock().unwrap();
+            guard.push_back(conf);
+        }
+        None => warn!("Received None!"), // TODO Why would this happen?
+    }
+}
+
 #[instrument(skip(rx, queue, queue_tx))]
 pub async fn profiling_task(
     rx: mpsc::Receiver<ProfilingConfiguration>,
@@ -57,54 +111,6 @@ pub async fn profiling_task(
 ) {
     // Using a tokio Mutex here to make it Send. Which is required...
     let rx = Arc::new(tokio::sync::Mutex::new(rx));
-    #[instrument(skip(queue, oneshot_tx, queue_tx))]
-    async fn work_on_queue(
-        queue: Arc<Mutex<VecDeque<ProfilingConfiguration>>>,
-        oneshot_tx: oneshot::Sender<()>,
-        queue_tx: mpsc::Sender<Job>,
-    ) {
-        fn pop_queue(
-            queue: Arc<Mutex<VecDeque<ProfilingConfiguration>>>,
-        ) -> Option<ProfilingConfiguration> {
-            let mut guard = queue.lock().unwrap();
-            guard.pop_front()
-        }
-        // TODO Do not clone the queue. It should be always the same arc, but how to make it work with the borrow checker?
-        // Probably by making the above function a closure, which would also be more idiomatic.
-        while let Some(current_conf) = pop_queue(queue.clone()) {
-            info!("Working on {current_conf:#?}...");
-            let report = compile_and_run(current_conf.clone()).await;
-            info!("Process completed with {report:?}.");
-            let finished_job = Job::Finished {
-                config: current_conf,
-                submitted: OffsetDateTime::now_utc(), // TODO Fix this.
-                runtime: Duration::new(5, 0), // TODO Get actual runtime from teebench output.
-                result: Ok(ReportWithFindings {
-                    report: report,
-                    findings: Vec::new(),
-                }),
-            };
-            queue_tx.send(finished_job).await.unwrap();
-        }
-        oneshot_tx.send(()).unwrap();
-    }
-    async fn receive_confs(
-        queue: Arc<Mutex<VecDeque<ProfilingConfiguration>>>,
-        rx: Arc<tokio::sync::Mutex<mpsc::Receiver<ProfilingConfiguration>>>,
-        queue_tx: mpsc::Sender<Job>,
-    ) {
-        let mut rx_guard = rx.lock().await;
-        match rx_guard.recv().await {
-            Some(conf) => {
-                info!("New task came in!");
-                let running_job = Job::Running(conf.clone());
-                queue_tx.send(running_job).await.unwrap();
-                let mut guard = queue.lock().unwrap();
-                guard.push_back(conf);
-            }
-            None => warn!("Received None!"), // TODO Why would this happen?
-        }
-    }
     loop {
         receive_confs(queue.clone(), rx.clone(), queue_tx.clone()).await;
         let (work_finished_tx, mut work_finished_rx) = oneshot::channel();
